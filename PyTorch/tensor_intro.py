1ee4de70d3af4a5a3a7b9369b40688643107535b
ryanavan7 <121054296+ryanavan7@users.noreply.github.com>
2025-04-18 20:02:30 -0700

    Datasets

--- PyTorch/tensor_intro.py
+++ PyTorch/tensor_intro.py
@@ -1,27 +1,27 @@
 import torch
 import numpy as np
 
-#data = [[1,2,6],[3,4,4]]
-#x_data = torch.tensor(data)
+data = [[1,2,6],[3,4,4]]
+x_data = torch.tensor(data)
 
-#np_array = np.array(data)
-#x_np = torch.from_numpy(np_array)
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
 
-#data1 = [[1,2],[0,0],[5,6],[7,0]]
-#x1_data = torch.tensor(data1)
+data1 = [[1,2],[0,0],[5,6],[7,0]]
+x1_data = torch.tensor(data1)
 
-#print(x_data)
-#print(x_np)
+print(x_data)
+print(x_np)
 
-#x_ones = torch.ones_like(x_data) # retains the properties of x_data
-#print(f"Ones Tensor: \n {x_ones} \n")
+x_ones = torch.ones_like(x_data) # retains the properties of x_data
+print(f"Ones Tensor: \n {x_ones} \n")
 
-#x_rand = torch.rand_like(x_np, dtype=torch.float) # overrides the datatype of x_data
-#print(f"Random Tensor: \n {x_rand} \n")
+x_rand = torch.rand_like(x_np, dtype=torch.float) # overrides the datatype of x_data
+print(f"Random Tensor: \n {x_rand} \n")
 
 shape = (3,4,)
 
-#rand_tensor = torch.rand(shape)
+rand_tensor = torch.rand(shape)
 #ones_tensor = torch.ones(shape)
 #zeros_tensor = torch.zeros(shape)
 
