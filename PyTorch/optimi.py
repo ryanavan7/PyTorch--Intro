967e4b4a239f7b3c168a74f7404887b72182aacd
ryanavan7 <121054296+ryanavan7@users.noreply.github.com>
2025-05-16 19:00:36 -0700

    optimization of nn

--- PyTorch/optimi.py
+++ PyTorch/optimi.py
@@ -40,61 +40,60 @@ class NeuralNetwork(nn.Module):
 
 model = NeuralNetwork()
 
+learning_rate = 1e-3
+batch_size = 64
+epochs = 5
+# Initialize the loss function
+loss_fn = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
 def train_loop(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     # Set the model to training mode - important for batch normalization and dropout layers
     # Unnecessary in this situation but added for best practices
     model.train()
     for batch, (X, y) in enumerate(dataloader):
-        #compute prediction and loss
+        # Compute prediction and loss
         pred = model(X)
         loss = loss_fn(pred, y)
 
-        #backpropagation
+        # Backpropagation
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * batch_size + len(X)
-            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-    def train_loop(dataloader, model, loss_fn, optimizer):
-        size = len(dataloader.dataset)
-        # Set the model to training mode - important for batch normalization and dropout layers
-        # Unnecessary in this situation but added for best practices
-        model.train()
-        for batch, (X, y) in enumerate(dataloader):
-            # Compute prediction and loss
+
+def test_loop(dataloader, model, loss_fn):
+    # Set the model to evaluation mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+    with torch.no_grad():
+        for X, y in dataloader:
             pred = model(X)
-            loss = loss_fn(pred, y)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
-            # Backpropagation
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-            if batch % 100 == 0:
-                loss, current = loss.item(), batch * batch_size + len(X)
-                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-
-    def test_loop(dataloader, model, loss_fn):
-        # Set the model to evaluation mode - important for batch normalization and dropout layers
-        # Unnecessary in this situation but added for best practices
-        model.eval()
-        size = len(dataloader.dataset)
-        num_batches = len(dataloader)
-        test_loss, correct = 0, 0
-
-        # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
-        # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
-        with torch.no_grad():
-            for X, y in dataloader:
-                pred = model(X)
-                test_loss += loss_fn(pred, y).item()
-                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-
-        test_loss /= num_batches
-        correct /= size
-        print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+epochs = 15
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
